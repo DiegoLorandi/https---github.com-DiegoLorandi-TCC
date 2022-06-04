2b56523c9f457a68bae85d0c6909bbbfc9fbad45
ciroesteves <ciro.esteves@hotmail.com>
2022-06-04 19:04:06 -0300

    Mudanças Ciro

--- src/pages/CrudAnimais/index.js
+++ src/pages/CrudAnimais/index.js
@@ -1,12 +1,15 @@
-import { View, Text, TextInput, TouchableOpacity, TouchableHighlight} from 'react-native'
-import React,{ useState } from 'react'
+import { View, Text, TextInput, TouchableOpacity, TouchableHighlight, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import {Picker} from '@react-native-picker/picker';
 import Radio from '../../components/Ratio'
 import { css } from './Css'
-import { cssGeral } from '../../assets/css/Css'
 import * as Animatable from 'react-native-animatable'
 
+
+
 export default function CrudAnimais() {
-  const [selected, setSelected]=useState(0);
+  const [selected, setSelected] = useState(0);
+  var state = {raca:''};
   return (
     <View style={css.container}>
       <Animatable.View animation="fadeInUp" delay={500}>
@@ -20,26 +23,29 @@ export default function CrudAnimais() {
         />
         <Text >Data de Nascimento</Text>
         <TextInput style={css.input}
-          keyboardType = 'numeric'
+          keyboardType='numeric'
           placeholder='00/00/0000'
         />
         <Text >Peso</Text>
         <TextInput style={css.input}
-          keyboardType = 'numeric'
+          keyboardType='numeric'
           placeholder='Kg:00,00'
         />
         <Text >Raça</Text>
-        <TextInput style={css.input}
-          placeholder='Digite a raça do animal'
-        />
+        <Picker>
+          <Picker.Item label="Selecione a raça" value="disabled" color='#aaa' />
+          <Picker.Item label="Nelore" value="Nelore" />
+          <Picker.Item label="Gir" value="Gir" />
+          <Picker.Item label="Guzerá" value="Guzerá" />
+        </Picker>
         <Text>Sexo</Text>
         <Radio
           selected={selected}
-          options={['Macho','Fêmea']} 
-          horizontal={true} 
-          onChangeSelect={(opt,index)=> {
-             setSelected(index);
-            }
+          options={['Macho', 'Fêmea']}
+          horizontal={true}
+          onChangeSelect={(opt, index) => {
+            setSelected(index);
+          }
           }
         />
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,5 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Login from '../pages/Login'
@@ -13,12 +12,68 @@ const Stack = createNativeStackNavigator();
 
 const Tab = createBottomTabNavigator();
 
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Details!</Text>
+    </View>
+  );
+}
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+function SettingsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+const HomeStack = createNativeStackNavigator();
+
+function HomeStackScreen() {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home" component={HomeScreen} />
+      <HomeStack.Screen name="Details" component={DetailsScreen} />
+    </HomeStack.Navigator>
+  );
+}
+
+const SettingsStack = createNativeStackNavigator();
+
+function SettingsStackScreen() {
+  return (
+    <SettingsStack.Navigator>
+      <SettingsStack.Screen name="Settings" component={SettingsScreen} />
+      <SettingsStack.Screen name="Details" component={DetailsScreen} />
+    </SettingsStack.Navigator>
+  );
+}
+
+
 export default function Routes() {
   return (
+    <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Login" component={Login} />
-        <Tab.Screen name="Welcome" component={Welcome} />
-        <Tab.Screen name="CrudAnimais" component={CrudAnimais} />
+        <Tab.Screen name="Home" component={HomeStackScreen} />
+        <Tab.Screen name="Settings" component={SettingsStackScreen} />
       </Tab.Navigator>
+    </NavigationContainer>
   );
 }
