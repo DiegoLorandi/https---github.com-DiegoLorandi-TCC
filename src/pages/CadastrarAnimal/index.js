3dc7754a7b2c599e98b781fe25c7b9151ec2c8ce
Diego <diego.lorandi@aluno.ifsp.edu.br>
2022-06-11 14:59:32 -0300

    CRUD Animais

--- src/pages/CadastrarAnimal/index.js
+++ src/pages/CadastrarAnimal/index.js
@@ -1,12 +1,101 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-
+import { db } from '../../../firebase'
+import { View, Text, TextInput, TouchableOpacity, TouchableHighlight } from 'react-native'
+import React, { useState } from 'react'
+import MaskInput from 'react-native-mask-input';
+import Radio from '../../components/Ratio'
+import { css } from './Css'
+import * as Animatable from 'react-native-animatable'
 
 
 const CadastrarAnimal = ({ navigation }) => {
+  const [selected, setSelected] = useState(0);
+  const [newIdAnimal, setNewIdAnimal] = useState("");
+  const [newPesoAnimal, setNewPesoAnimal] = useState();
+  const [newDataAnimal, setNewDataAnimal] = useState("");
+  const [newRacaAnimal, setNewRacaAnimal] = useState("");
+  const [newSexoAnimal, setNewSexoAnimal] = useState("Macho");
+  const [newStatusAnimal, setNewStatusAnimal] = useState("");
+  const Create = async () => {
+    const animaisCollection = db.collection('animais');
+    if (newIdAnimal == "" || newPesoAnimal == "" || newDataAnimal == "" || newRacaAnimal == "" || newSexoAnimal == "" || newStatusAnimal == "") {
+      alert("Preencha todos os dados corretamente")
+    } else {
+      try {
+        await animaisCollection.add({
+          idAnimal: newIdAnimal,
+          dataNascimento: newDataAnimal,
+          pesoAnimal: newPesoAnimal,
+          sexoAnimal: newSexoAnimal,
+          racaAnimal: newRacaAnimal,
+          statusAnimal: newStatusAnimal,
+        });
+        alert("Animal cadastrado")
+      } catch (error) {
+        alert(error.message)
+      }
+    }
+  }
+
   return (
-    <View >
-      <Text>Cadastrar Animal</Text>
+    <View style={css.container}>
+      <Animatable.View animation="fadeInUp" delay={500}>
+        <Text>Cadastro de Animais</Text>
+      </Animatable.View>
+
+      <Animatable.View animation="fadeInUp" style={css.formLogin}>
+        <Text >Id</Text>
+        <TextInput style={css.input}
+          placeholder='Digite o Id do animal'
+          onChangeText={value => setNewIdAnimal(value)}
+        />
+        <Text >Data de Nascimento</Text>
+        <MaskInput
+          value={newDataAnimal}
+          onChangeText={(masked, unmasked) => {
+            setNewDataAnimal(masked);
+          }}
+          mask={[/\d/, /\d/, '/', /\d/, /\d/, '/', /\d/, /\d/, /\d/, /\d/]}
+        />
+
+
+
+        <Text >Peso</Text>
+        <TextInput style={css.input}
+          keyboardType='numeric'
+          placeholder='Kg:000.00'
+          onChangeText={value => setNewPesoAnimal(value)}
+        />
+        <Text >Raça</Text>
+        <TextInput style={css.input}
+          placeholder='Digite a raça do animal'
+          onChangeText={value => setNewRacaAnimal(value)}
+        />
+        <Text>Sexo</Text>
+        <Radio
+          selected={selected}
+          options={['Macho', 'Fêmea']}
+          horizontal={true}
+          onChangeSelect={(options, index) => {
+            setNewSexoAnimal(options)
+            setSelected(index);
+          }
+          }
+        />
+
+        <Text >Status</Text>
+        <TextInput style={css.input}
+          placeholder='Selecione o status do animal'
+          onChangeText={value => setNewStatusAnimal(value)}
+        />
+
+        <View >
+          <TouchableOpacity
+            onPress={Create}>
+            <Text>Cadastrar Animal</Text>
+          </TouchableOpacity>
+        </View>
+      </Animatable.View>
+
     </View>
   );
 }
--- src/pages/ConsultarAnimal/index.js
+++ src/pages/ConsultarAnimal/index.js
@@ -1,11 +1,160 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import * as Animatable from 'react-native-animatable'
 import { css } from './Css'
+import { db } from '../../../firebase'
+
 
 const ConsultarAnimal = ({ navigation }) => {
+  const [editable, setEditable] = useState(false);
+  const [fetchIdAnimal, setFetchIdAnimal] = useState("");
+  const [showDataAnimal, setShowDataAnimal] = useState("");
+  const [showPesoAnimal, setShowPesoAnimal] = useState("");
+  const [showSexoAnimal, setShowSexoAnimal] = useState("");
+  const [showRacaAnimal, setShowRacaAnimal] = useState("");
+  const [showStatusAnimal, setShowStatusAnimal] = useState("");
+
+
+  const [newDataAnimal, setNewDataAnimal] = useState(null);
+  const [newPesoAnimal, setNewPesoAnimal] = useState(null);
+  const [newSexoAnimal, setNewSexoAnimal] = useState(null);
+  const [newRacaAnimal, setNewRacaAnimal] = useState(null);
+  const [newStatusAnimal, setNewStatusAnimal] = useState(null);
+
+
+  const Read = async () => {
+    const animaisCollection = db.collection('animais');
+    try {
+      var getAnimais = await animaisCollection.where('idAnimal', '==', '' + fetchIdAnimal + '').get()
+      var dataAnimalBuscado
+      var pesoAnimalBuscado
+      var sexoAnimalBuscado
+      var racaAnimalBuscado
+      var statusAnimalBuscado
+      getAnimais.forEach(doc => {
+        dataAnimalBuscado = doc.data().dataNascimento;
+        pesoAnimalBuscado = doc.data().pesoAnimal;
+        sexoAnimalBuscado = doc.data().sexoAnimal;
+        racaAnimalBuscado = doc.data().racaAnimal;
+        statusAnimalBuscado = doc.data().statusAnimal;
+      });
+      setShowDataAnimal(dataAnimalBuscado)
+      setShowPesoAnimal(pesoAnimalBuscado)
+      setShowSexoAnimal(sexoAnimalBuscado)
+      setShowRacaAnimal(racaAnimalBuscado)
+      setShowStatusAnimal(statusAnimalBuscado)
+      setEditable(true)
+    } catch (error) {
+      alert(error.message)
+    }
+  }
+  const Update = async () => {
+      const animaisCollection = db.collection('animais');
+      var getAnimais = await animaisCollection.where('idAnimal', '==', '' + fetchIdAnimal + '').get().then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            doc.ref.update({dataNascimento: newDataAnimal})
+            doc.ref.update({pesoAnimal: newPesoAnimal})
+            doc.ref.update({racaAnimal: newRacaAnimal})
+            doc.ref.update({sexoAnimal: newSexoAnimal})
+            doc.ref.update({statusAnimal: newStatusAnimal})
+        })
+      })
+  }
+  const Delete = async () => {
+    try {
+      const animaisCollection = db.collection('animais');
+      const getAnimais =  animaisCollection.where('idAnimal', '==', '' + fetchIdAnimal + '');
+      await getAnimais.get().then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+          doc.ref.delete();
+        });
+      });
+      setFetchIdAnimal("")
+      setShowDataAnimal("")
+      setShowPesoAnimal("")
+      setShowSexoAnimal("")
+      setShowRacaAnimal("")
+      setShowStatusAnimal("")
+      setEditable(false)
+    } catch (error) {
+      alert(error.message)
+    }
+  }
+  console.log("OnChange Peso:"+newPesoAnimal)
   return (
     <View >
       <Text>Consultar Animais</Text>
+      <Animatable.View animation="fadeInUp" style={css.formLogin}>
+        <Text >Id do Animal:</Text>
+        <TextInput style={css.input}
+          keyboardType='numeric'
+          placeholder='Digite o Id do animal'
+          value={fetchIdAnimal}
+          onChangeText={value => setFetchIdAnimal(value)}
+        />
+        <Text >Data de Nascimento:</Text>
+        <TextInput
+          editable={editable}
+          multiline={false}
+          defaultValue={showDataAnimal}
+          value={newDataAnimal} 
+          onChangeText={text => setNewDataAnimal(text)}
+        />
+        <Text >Peso:</Text>
+        <TextInput
+         editable={editable}
+          multiline={false}
+          defaultValue={showPesoAnimal}
+          value={newPesoAnimal} 
+          onChangeText={text => setNewPesoAnimal(text)}
+          />
+        
+        <Text >Sexo do Animal:</Text>
+        <TextInput
+         editable={editable}
+          multiline={false}
+          defaultValue={showSexoAnimal}
+          value={newSexoAnimal} 
+          onChangeText={text => setNewSexoAnimal(text)}
+          />
+        
+        <Text >Raca:</Text>
+        <TextInput
+         editable={editable}
+          multiline={false}
+          defaultValue={showRacaAnimal}
+          value={newRacaAnimal} 
+          onChangeText={text => setNewRacaAnimal(text)}
+          />
+        
+        <Text >Status:</Text>
+        <TextInput
+         editable={editable}
+          multiline={false}
+          defaultValue={showStatusAnimal}
+          value={newStatusAnimal} 
+          onChangeText={text => setNewStatusAnimal(text)}
+          />
+        
+        <View >
+          <TouchableOpacity
+            onPress={Read}>
+            <Text>Buscar Animal</Text>
+          </TouchableOpacity>
+
+
+          <TouchableOpacity
+            onPress={Update}>
+            <Text>Editar dados</Text>
+          </TouchableOpacity>
+
+
+          <TouchableOpacity
+            onPress={Delete}>
+            <Text>Excluir Animal</Text>
+          </TouchableOpacity>
+        </View>
+      </Animatable.View>
     </View>
   );
 }
